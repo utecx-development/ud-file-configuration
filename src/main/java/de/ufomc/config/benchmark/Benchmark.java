fdd0cf5632a62df316ff5cd44bf7f1246731956b
Waischbrot <rubymc@gmx.de>
2024-09-10 12:39:40 +0200

    fix Benchmark#getUsedMemory and mark as Lombok @UtilityClass

--- src/main/java/de/ufomc/config/benchmark/Benchmark.java
+++ src/main/java/de/ufomc/config/benchmark/Benchmark.java
@@ -4,18 +4,21 @@ import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.List;
 
-public class Benchmark {
+@UtilityClass
+public final class Benchmark {
 
+    /**
+     * Used for getting the amount of memory this runtime is using at this moment.
+     * @return amount of memory as long
+     */
     private static long getUsedMemory() {
-        Runtime runtime = Runtime.getRuntime();
-        return runtime.freeMemory();
+        final Runtime runtime = Runtime.getRuntime();
+        return runtime.totalMemory() - runtime.freeMemory(); //subtract free memory from total
     }
-
-
 
     private static long getGCCount() {
         long totalGCCount = 0;
-        List<GarbageCollectorMXBean> gcBeans = ManagementFactory.getGarbageCollectorMXBeans();
+        final List<GarbageCollectorMXBean> gcBeans = ManagementFactory.getGarbageCollectorMXBeans();
         for (GarbageCollectorMXBean gcBean : gcBeans) {
             long count = gcBean.getCollectionCount();
             if (count != -1) {
@@ -48,5 +51,4 @@ public class Benchmark {
         return new BenchmarkResult(totalTime, iterations, memoryUsedBefore, memoryUsedAfter);
 
     }
-
 }
