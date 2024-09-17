41ff0df9edc165009d1e9d2474cff02e8b70712b
Waischbrot <rubymc@gmx.de>
2024-09-17 12:31:02 +0200

    replace constructor infrastructure against clean factory methods & javadoc comments

--- src/main/java/dev/ufo/ufodata/core/io/UfoFile.java
+++ src/main/java/dev/ufo/ufodata/core/io/UfoFile.java
@@ -9,43 +9,67 @@ import dev.ufo.ufodata.core.format.ObjectFormatter;
 import dev.ufo.ufodata.lib.TypeValue;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public final class UfoFile {
-    File file;
-    @Getter Map<String, TypeValue> cache;
-    @NonFinal boolean prettyWriting;
+    private static final String FILE_SUFFIX = ".ud";
+    @Getter File file;
+    Map<String, TypeValue> cache; //no getter since this would be a vulnerability (some data might not be saved)
+    @Getter @Setter @NonFinal boolean prettyWriting;
+    @Getter @Setter @NonFinal boolean changed; //if this has not changed there is no need to write into the file!
 
-    public UfoFile(String fileName, Class<?> main) {
-        try {
-            this.file = new File(new File(main.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent(), fileName + ".ud");
-        } catch (Exception e) {
-            throw new RuntimeException("Could not find an execution path!", e);
-        }
-
-        this.cache = FileManager.init(file);
-    }
-
-    public UfoFile(String path, String fileName) {
-        this.file = new File(path, fileName + ".ud");
-        this.cache = FileManager.init(file);
-    }
-
-    public UfoFile(File file) {
+    //private constructor getting the file directly
+    private UfoFile(File file) {
         this.file = file;
         this.cache = FileManager.init(file);
     }
 
-    public UfoFile prettyWriting(){
-        this.prettyWriting = true;
-        return this;
+    /**
+     * Creates a new instance with the given file.
+     * @param file The file to work with
+     * @return new UfoFile instance
+     */
+    @NonNull
+    public static UfoFile of(final File file) {
+        return new UfoFile(file);
+    }
+
+    /**
+     * Creates a new instance by a file found through a path and a filename.
+     * @param path Folder the file should be in
+     * @param fileName Name of the file to work with
+     * @return new UfoFile instance
+     */
+    @NonNull
+    public static UfoFile of(final String path, final String fileName) {
+        final File file = new File(path, fileName + FILE_SUFFIX);
+        return UfoFile.of(file);
+    }
+
+    /**
+     * Creates a new instance by inputting a class, effectively trying to find its location and working in its directory
+     * @param main Class the location of is found and file created next to.
+     * @param fileName Name of the file to work with
+     * @return new UfoFile instance (if successful)
+     * @throws RuntimeException Thrown in case there is a problem with the given main class
+     */
+    public static UfoFile of(final Class<?> main, final String fileName) throws RuntimeException {
+        try {
+            final String path = new File(main.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
+            return UfoFile.of(path, fileName);
+        } catch (URISyntaxException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 
     public <K, V> Map<K, V> getMap(Class<K> keyClazz, Class<V> valueClazz, String key) {
