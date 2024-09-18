6239107f152ab277741a9513f3d890d43cb50cee
Waischbrot <rubymc@gmx.de>
2024-09-18 13:14:49 +0200

    rework UfoSerializer#deserialize

--- src/main/java/dev/ufo/ufodata/core/io/UfoSerializer.java
+++ src/main/java/dev/ufo/ufodata/core/io/UfoSerializer.java
@@ -15,35 +15,38 @@ import java.util.Map;
 public final class UfoSerializer {
 
     /**
-     * 
-     * @param file
-     * @return
+     * Deserializes a files contents to a map usable by this parser.
+     * @param file The file to read from.
+     * @return Hashmap in a key -> value structure
      */
     @NonNull
     public static Map<String, TypeValue> deserialize(final File file) {
-        if (!file.exists()) initFile(file);
+        if (!file.exists()) initFile(file); //create the file if it does not exist yet
+
         try (final BufferedReader reader = new BufferedReader(new FileReader(file))) {
             final Map<String, TypeValue> map = new HashMap<>();
-            final StringBuilder builder = new StringBuilder();
 
+            //read all of the files contents
+            final StringBuilder builder = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
                 if (line.isEmpty()) continue;
                 if (line.startsWith("#")) continue;
                 builder.append(line);
             }
+            final String[] lines = builder.toString().replace("\n", "").split(";"); //remove formatting tags
 
             try {
+                for (final String current : lines) {
+                    if (current.isEmpty()) continue; //empty line in file
 
-                final String[] lines = builder.toString().replace("\n", "").split(";");
-                for (final String current : lines){
-                    if (current.isEmpty()) continue;
-
+                    //parse key - value
                     final String[] typeRest = current.split(":");
                     final String type = typeRest[0];
                     final String key = typeRest[1].split("=")[0];
                     final String value = typeRest[1].split("=")[1];
 
+                    //format
                     if (type.startsWith("map<")) {
                         map.put(key, new TypeValue(type, MapFormatter.formatMap(type, value)));
                     } else if (type.startsWith("list<")) {
@@ -54,7 +57,7 @@ public final class UfoSerializer {
                 }
                 return map;
 
-            } catch (final Exception exception){
+            } catch (final Exception exception) {
                 throw new RuntimeException("Failed to load file: '" + file.getParent() + "\\" + file.getName() + "' because of wrong syntax");
             }
         } catch (final IOException exception) {
