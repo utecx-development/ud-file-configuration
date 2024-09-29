fd1e68c51ee17adb392e7a985ecb7d7f06a68aac
Waischbrot <rubymc@gmx.de>
2024-09-29 13:07:18 +0200

    more tests

--- src/test/java/dev/ufo/CheckTest.java
+++ src/test/java/dev/ufo/CheckTest.java
@@ -4,15 +4,33 @@ import dev.ufo.ufodata.core.io.UfoFile;
 import dev.ufo.ufodata.lib.checks.CheckType;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public final class CheckTest {
 
     @Test
-    public void testTypeChecks() {
+    public void testPrimitiveCheck() {
         assertFalse(CheckType.isNotPrimitive(Integer.class));
+
         assertFalse(CheckType.isNotPrimitive(int.class));
+
         assertTrue(CheckType.isNotPrimitive(UfoFile.class));
+    }
+
+    @Test
+    public void testListOrMapCheck() {
+        final List<String> list = new ArrayList<>();
+        assertTrue(CheckType.isListOrMap(list.getClass()));
+
+        final Map<String, Integer> map = new HashMap<>();
+        assertTrue(CheckType.isListOrMap(map.getClass()));
+
+        assertFalse(CheckType.isListOrMap(UfoFile.class));
     }
 }
