a2ea73b8a9f1d6c47df00def14777206aeefca1c
Waischbrot <rubymc@gmx.de>
2024-09-29 12:37:57 +0200

    example tests

--- src/test/java/dev/ufo/CheckTest.java
+++ src/test/java/dev/ufo/CheckTest.java
@@ -1,12 +1,18 @@
 package dev.ufo;
 
+import dev.ufo.ufodata.core.io.UfoFile;
+import dev.ufo.ufodata.lib.checks.CheckType;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public final class CheckTest {
 
     @Test
     public void testTypeChecks() {
-        
+        assertTrue(CheckType.isPrimitive(Integer.class));
+        assertTrue(CheckType.isPrimitive(int.class));
+        assertFalse(CheckType.isPrimitive(UfoFile.class));
     }
-
 }
